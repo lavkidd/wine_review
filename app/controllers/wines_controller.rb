7f8cfa391675d143e81161ee07a80fa6a6eecd95
Lora Vernon-Kidd <lav4@hotmail.com>
2014-09-29 01:05:05 -0400

    Save 1

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,63 @@
 class WineController < ApplicationController
   def index
+  	@available_at = Time.now
+  	@wines = ["Chardonnay", "Cuvee Noir", "Pinot Gris", "Sunblush"]
   end
 end
 
+#class BooksController < ApplicationController
+#
+#	def index
+#	   @available_at =  Time.now
+#	   @books = Book.order(:title).page(params[:page])
+# 	end
+#
+# 	before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+#
+# 	def set_book
+#	 	@book = Book.find(params[:id])
+#	 end
+#
+# 	def show
+#	 	@book = Book.find(params[:id])
+# 	end
+#
+#	def new
+#		@book = Book.new
+#	end
+#
+#	def create
+#		@book = Book.new(book_params)
+#		if @book.save
+#		  redirect_to @book, notice: "#{@book.title} was created!!"
+#		else
+#		  render :new
+#		end
+#	end
+#
+#	def edit
+#		@book = Book.find(params[:id])
+#	end
+#
+#	def update
+#		@book = Book.find(params[:id])
+#		if @book.update(book_params)
+#		  redirect_to @book, notice: "#{@book.title} was updated!!"
+#		else
+#		  render :new
+#		end
+#	end
+#
+#	def destroy
+##		@book = Book.find(params[:id])
+#		@book.destroy
+#		redirect_to books_url
+#	end
+#
+# 	private
+#
+#	def book_params
+#		params.require(:book).permit(:title, :author, :pages, :price)
+#	end
+#
+#end
